1518299f1a755470a0029f73f7c216027d15113b
lixing <1291729176@qq.com>
2026-05-14 15:13:11 +0800

    verify 69

---
+++ QCP_examples/humaneval/IntArrayClaude/C_69_proof_auto.v
@@ -0,0 +1,165 @@
+Require Import Coq.ZArith.ZArith.
+Require Import Coq.Bool.Bool.
+Require Import Coq.Strings.String.
+Require Import Coq.Lists.List.
+Require Import Coq.Classes.RelationClasses.
+Require Import Coq.Classes.Morphisms.
+Require Import Coq.micromega.Psatz.
+Require Import Coq.Sorting.Permutation.
+From AUXLib Require Import int_auto Axioms Feq Idents ListLib VMap.
+Require Import SetsClass.SetsClass. Import SetsNotation.
+From SimpleC.SL Require Import Mem SeparationLogic.
+From SimpleC.EE Require Import C_69_goal.
+Require Import Logic.LogicGenerator.demo932.Interface.
+Local Open Scope Z_scope.
+Local Open Scope sets.
+Local Open Scope string.
+Local Open Scope list.
+Import naive_C_Rules.
+Require Import coins_69.
+Local Open Scope sac.
+
+Lemma proof_of_search_safety_wit_1 : search_safety_wit_1.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_2 : search_safety_wit_2.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_3 : search_safety_wit_3.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_4 : search_safety_wit_4.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_5 : search_safety_wit_5.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_6 : search_safety_wit_6.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_7 : search_safety_wit_7.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_8 : search_safety_wit_8.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_9 : search_safety_wit_9.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_10 : search_safety_wit_10.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_11 : search_safety_wit_11.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_12 : search_safety_wit_12.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_13 : search_safety_wit_13.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_15 : search_safety_wit_15.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_16 : search_safety_wit_16.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_17 : search_safety_wit_17.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_18 : search_safety_wit_18.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_19 : search_safety_wit_19.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_20 : search_safety_wit_20.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_21 : search_safety_wit_21.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_22 : search_safety_wit_22.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_23 : search_safety_wit_23.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_24 : search_safety_wit_24.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_25 : search_safety_wit_25.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_26 : search_safety_wit_26.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_27 : search_safety_wit_27.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_28 : search_safety_wit_28.
+Proof. Admitted. 
+
+Lemma proof_of_search_safety_wit_29 : search_safety_wit_29.
+Proof. Admitted. 
+
+Lemma proof_of_search_entail_wit_5 : search_entail_wit_5.
+Proof. Admitted. 
+
+Lemma proof_of_search_entail_wit_9 : search_entail_wit_9.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_1_pure : search_partial_solve_wit_1_pure.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_1 : search_partial_solve_wit_1.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_2_pure : search_partial_solve_wit_2_pure.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_2 : search_partial_solve_wit_2.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_3 : search_partial_solve_wit_3.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_4 : search_partial_solve_wit_4.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_5 : search_partial_solve_wit_5.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_6 : search_partial_solve_wit_6.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_7 : search_partial_solve_wit_7.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_8 : search_partial_solve_wit_8.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_9 : search_partial_solve_wit_9.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_10 : search_partial_solve_wit_10.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_11 : search_partial_solve_wit_11.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_12 : search_partial_solve_wit_12.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_13_pure : search_partial_solve_wit_13_pure.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_13 : search_partial_solve_wit_13.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_14_pure : search_partial_solve_wit_14_pure.
+Proof. Admitted. 
+
+Lemma proof_of_search_partial_solve_wit_14 : search_partial_solve_wit_14.
+Proof. Admitted. 
+
